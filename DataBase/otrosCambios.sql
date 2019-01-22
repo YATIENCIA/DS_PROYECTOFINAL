f928980b01cb7e817abb5e1204e8cced072646a7
YATIENCIA <33614080+YATIENCIA@users.noreply.github.com>
2019-01-21 20:58:16 -0500

    Agregar otros procedures necesarios

---
+++ DataBase/otrosCambios.sql
@@ -0,0 +1,32 @@
+use poliventas;
+
+create table busqueda(
+idProd int,
+numero int auto_increment,
+primary key(numero),
+foreign key	(idProd) references producto(idproducto));
+
+drop procedure busquedaProd;
+delimiter $$
+create procedure busquedaProd(in busqueda varchar(100))
+begin 
+select nombre,categoria,  costo, cantidadDisponible, tiempoMaxEntrega
+from producto
+
+where (nombre like CONCAT('%', busqueda, '%') or categoria like CONCAT('%', busqueda, '%')) and eliminado=false;
+
+insert into busqueda(idProd) select idproducto from producto
+
+where (nombre like CONCAT('%', busqueda, '%') or categoria like CONCAT('%', busqueda, '%')) and eliminado=false;
+
+end$$
+
+delimiter ;
+
+select nombre, costo, sq.cantidad as cantidadBusquedas
+from producto
+join (select count(numero) as cantidad, idProd
+		from busqueda
+        group by idProd) sq on producto.idproducto=sq.idProd
+order by cantidad desc
+limit 15 ;
